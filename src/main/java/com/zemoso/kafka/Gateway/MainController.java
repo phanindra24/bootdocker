7b5b85eab3e831e7da681fbea68b1274413cc195
Phanindra <phanindraiitb@gmail.com>
2017-09-20 11:52:53 +0530

    Initial Commit Kafka Spring Boot Producer and Consumer

--- src/main/java/com/zemoso/kafka/Gateway/MainController.java
+++ src/main/java/com/zemoso/kafka/Gateway/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
+
     @Autowired
     private KafkaProducer kafkaProducer;
 
--- src/test/java/com/zemoso/kafka/SpringKafkaTemplateApplicationTests.java
+++ src/test/java/com/zemoso/kafka/SpringKafkaTemplateApplicationTests.java
@@ -1,65 +1,80 @@
-//package com.zemoso.kafka;
-//
-//import com.zemoso.kafka.Consumer.KafkaConsumer;
-//import com.zemoso.kafka.Producer.KafkaProducer;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.ClassRule;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
-//import org.springframework.kafka.listener.MessageListenerContainer;
-//import org.springframework.kafka.test.rule.KafkaEmbedded;
-//import org.springframework.kafka.test.utils.ContainerTestUtils;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SpringKafkaTemplateApplicationTests {
-//
-//	private static String BOOT_TOPIC = "boot.t";
-//
-//	@Autowired
-//	private KafkaProducer kafkaProducer;
-//
-//	@Autowired
-//	private KafkaConsumer kafkaConsumer;
-//
-//	@Autowired
-//	private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
-//
-//	/*@ClassRule
-//	*//**
-//	 * embedded Kafka broker which is started via a JUnit @ClassRule.
-//	 *//*
-//	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, BOOT_TOPIC);*/
-//
-//	/*@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//		System.setProperty("spring.kafka.bootstrap-servers", embeddedKafka.getBrokersAsString());
-//	}*/
-//
-//	/*@Before
-//	public void setUp() throws Exception {
-//		// wait until the partitions are assigned
-//		for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry
-//				.getListenerContainers()) {
-//			ContainerTestUtils.waitForAssignment(messageListenerContainer,
-//					embeddedKafka.getPartitionsPerTopic());
-//		}
-//	}*/
-//
-//	@Test
-//	public void testReceive() throws Exception {
-//		kafkaProducer.send(BOOT_TOPIC, "Hello Boot!");
-//
-//		kafkaConsumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
-//		assertThat(kafkaConsumer.getLatch().getCount()).isEqualTo(0);
-//	}
-//
-//}
+package com.zemoso.kafka;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.zemoso.kafka.Consumer.KafkaConsumer;
+import com.zemoso.kafka.Producer.KafkaProducer;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKafkaTemplateApplicationTests {
+
+	private static String BOOT_TOPIC = "mirthProducertest4";
+
+	@Autowired
+	private KafkaProducer kafkaProducer;
+
+	@Autowired
+	private KafkaConsumer kafkaConsumer;
+
+	@Autowired
+	private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+	/*@ClassRule
+	*//**
+	 * embedded Kafka broker which is started via a JUnit @ClassRule.
+	 *//*
+	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, BOOT_TOPIC);*/
+
+	/*@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.setProperty("spring.kafka.bootstrap-servers", embeddedKafka.getBrokersAsString());
+	}*/
+
+	/*@Before
+	public void setUp() throws Exception {
+		// wait until the partitions are assigned
+		for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry
+				.getListenerContainers()) {
+			ContainerTestUtils.waitForAssignment(messageListenerContainer,
+					embeddedKafka.getPartitionsPerTopic());
+		}
+	}*/
+
+	@Test
+	public void testReceive() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+
+		ArrayNode arrayNode = mapper.createArrayNode();
+
+		/**
+		 * Create three JSON Objects objectNode1, objectNode2, objectNode3
+		 * Add all these three objects in the array
+		 */
+
+		ObjectNode objectNode1 = mapper.createObjectNode();
+		objectNode1.put("bookName", "Java");
+		objectNode1.put("price", "100");
+		kafkaProducer.send(BOOT_TOPIC, objectNode1);
+
+		kafkaConsumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		assertThat(kafkaConsumer.getLatch().getCount()).isEqualTo(0);
+	}
+
+}
